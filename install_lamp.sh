3c4259ca2c787dd2e8d88b6f1ab8b988776f667c
Luiz Alberto Mesquita <luizalbertobm@gmail.com>
2020-07-29 02:32:16 +0100

    add comments

--- install_lamp.sh
+++ install_lamp.sh
@@ -2,7 +2,11 @@
 #=============================
 #install lamp 
 
-#sudo apt update
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/apache2
+
 echo '==================================='
 echo 'installing apache'
 sudo apt install -y apache2
@@ -19,20 +23,23 @@ curl http://icanhazip.com
 echo '==================================='
 echo 'Installing Mysql'
 sudo apt-get install -y mysql-server
+git clone https://github.com/pematon/adminer-custom.git _adminer
+#mkdir /var/www/html/adminer
+#wget https://www.adminer.org/latest.php -O /var/www/html/adminer/adminer.php
 
 echo '==================================='
 echo 'Installing PHP'
-sudo apt-get install -y php libapache2-mod-php
-sudo apt-get install -y php-cli hhvm
+sudo apt-get install -y php7.4 libapache2-mod-php php-cli hhvm
 
 #install php modules
-sudo apt-get install -y  php-mysql php-imagick php-xdebug
+sudo apt install php7.4-xdebug php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl -y
+
 
 #configure file order
 echo '==================================='
 echo 'Changing index.php file order'
-sudo sed -i 's/index.php //g' /etc/apache2/mods-enabled/dir.conf
-sudo sed -i 's/index.html/index.php index.html/g' /etc/apache2/mods-enabled/dir.conf
+#sudo sed -i 's/index.php //g' /etc/apache2/mods-enabled/dir.conf
+#sudo sed -i 's/index.html/index.php index.html/g' /etc/apache2/mods-enabled/dir.conf
 
 echo '==================================='
 echo 'Configuring PHP errors'
@@ -42,3 +49,5 @@ sudo sed -i 's/display_startup_errors = Off/display_startup_errors = On/g' /etc/
 #restart apache
 sudo service apache2 restart
 sudo service apache2 status
+
+echo "Use 'update-alternatives --set php' to change php version"
--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #=============================
+sudo apt update
+sudo apt upgrade
+
+source ./install_lamp.sh
+source ./install_composer.sh
+source ./install_git.sh
+source ./install_flutter.sh
 source ./install_node.sh
+sudo snap install android-studio
